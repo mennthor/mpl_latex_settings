36c3d985a1daa3ce20cfe7fc3da70b4589291b17
Thorben Menne <mennthor@aol.com>
2018-08-24 15:56:06 +0200

    Using a simple config file now, so only these can get version controlled in the prohect repos and this module need to be installed only once

--- mpl_latex_settings/__init__.py
+++ mpl_latex_settings/__init__.py
@@ -1,24 +1,52 @@
 # Expose global LaTeX plot settings
 from .publication_ready_plot_settings import LatexSettings
+import json as _json
 
 
-def create_latex_setup(interactive=True):
+def create_latex_setup(config, interactive=True):
     """
     Create the LaTeX setup.
 
     Parameters
     ----------
+    config : string
+        Path to a JSON config file, to set up the basic settings.
     interactive : bool, optional
         If ``True`, setup is for interactive use, by registering the ``'pdf'``
         backend. Else, the full ``'pgf'`` backend is used.
     """
+    with open(config, "r") as fp:
+        config = _json.load(fp)
+
+    required_keys = [
+        "textwidth_inches",
+        "texengine",
+        ]
+    opt_keys = {
+        "prim_fontsize_pt": 11,
+        "sec_fontsize_pt": 9,
+        "latex_args": [],
+        }
+
+    settings = {}
+    for key in required_keys:
+        if key in config:
+            settings[key] = config.pop(key)
+        else:
+            raise KeyError("Config file is missing required key " +
+                           "'{}'.".format(key))
+    for key, val in opt_keys.items():
+        settings[key] = config.pop(key, val)
+    # Info, when extra, unused keys are left
+    if config:
+        raise KeyError("Keys ['{}'] not used from given config.".format(
+            "', '".join(list(config.keys()))))
+
+    settings["interactive"] = interactive
     latex_setup = LatexSettings(
-        textwidth_inches=5.78853,
-        texengine="lualatex",
-        interactive=interactive,
-        prim_fontsize_pt=10.95,
-        sec_fontsize_pt=9,
-        latex_args={
-            "text.latex.preamble": [r"\usepackage[locale=US]{siunitx}"]},
-    )
+        textwidth_inches=settings.pop("textwidth_inches"),
+        texengine=settings.pop("texengine"),
+        **settings
+        )
+
     return latex_setup
